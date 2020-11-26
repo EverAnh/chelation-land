c04b0ec152f763c0766e8b93256c460d5c6bfcc1
Anh Nguyen <anhhn1@gmail.com>
2020-11-26 01:17:48 -0800

    visual tweaks after looking on my physical phone

--- assets/jss/components/layoutStyle.js
+++ assets/jss/components/layoutStyle.js
@@ -3,15 +3,14 @@ import { title } from "assets/jss/nextjs-material-kit.js";
 const layoutStyle = {
   title: {
     ...title,
-    fontSize: "3rem",
+    fontSize: "2.441rem",
     color: "#f7f7f7",
     display: "inline-block",
     marginTop: "30px",
     minHeight: "32px",
     position: "relative",
     zIndex: "3",
-    //textShadow: "2px 4px 2px #9379cc, 2px 5px 1px #9379cc, 2px 3px 5px #9379cc"
-    textShadow: "2px 4px 2px black, 2px 5px 1px black, 2px 3px 5px black"
+    textShadow: "1px 1px 0px #9379cc, -1px -1px 0px #9379cc, 2px 5px 1px #9379cc"
   }
 };
 
