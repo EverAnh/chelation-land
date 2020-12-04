cc0762aee47ada5bbb5aaa457e48876dea09f0b9
Anh Nguyen <anhhn1@gmail.com>
2020-12-04 01:57:40 -0800

    events data update

--- data/events.js
+++ data/events.js
@@ -44,7 +44,7 @@ const events = [
     moment: moment.parseZone("12/11/2020 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
-    name: "Shop Expires: Mirrors Selection Ticket",
+    name: "Shops Expire: Mirrors Ranking, Mirrors Selection Ticket",
     categ: "shopExpire",
     future: false,
     link: null,
@@ -53,21 +53,28 @@ const events = [
   {
     name: "End of: Angels On The Road",
     categ: "eventEnd",
-    future: true,
+    future: false,
     link: "http://jp.rika.ren/pop_up.html#id=2178",
     moment: moment.parseZone("12/16/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
     name: "End of: Himika Mao Gacha",
     categ: "gacha",
-    future: true,
+    future: false,
     link: "http://jp.rika.ren/pop_up.html#id=2176",
     moment: moment.parseZone("12/16/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
+    name: "Shops Expire: Angels On The Road",
+    categ: "shopExpire",
+    future: false,
+    link: null,
+    moment: moment.parseZone("12/23/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
     name: "End of: Tsuruno / Felicia Gacha",
     categ: "gacha",
-    future: true,
+    future: false,
     link: "http://jp.rika.ren/pop_up.html#id=2177",
     moment: moment.parseZone("12/28/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
@@ -77,6 +84,13 @@ const events = [
     future: false,
     link: null,
     moment: moment.parseZone("12/31/2020 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Shops Expire: Tsuruno / Felicia Gacha Coins",
+    categ: "shopExpire",
+    future: false,
+    link: null,
+    moment: moment.parseZone("1/4/2021 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
     name: "Kimochi Battle - Vigorous Opal Neck",
