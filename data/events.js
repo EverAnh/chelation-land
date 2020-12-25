b988cec585be4625a5b6b1eb6a23ef4233790169
Anh Nguyen <anhhn1@gmail.com>
2020-12-25 02:37:56 -0800

    events data update

--- data/events.js
+++ data/events.js
@@ -2,53 +2,11 @@ import moment from "moment";
 
 const events = [
   {
-    name: "Start of: Dec 12 Maintenance",
-    categ: "maintenance",
-    future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2201",
-    moment: moment.parseZone("12/21/2020 15:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
-    name: "End of: Dec 12 Maintenance",
-    categ: "maintenance",
-    future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2201",
-    moment: moment.parseZone("12/21/2020 17:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
-    name: "Start of: Kimochi Battle - Vigorous Opal Neck",
-    categ: "eventStart",
-    future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2200",
-    moment: moment.parseZone("12/21/2020 17:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
-    name: "Start of: Christmas String, part 2",
-    categ: "eventStart",
-    future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2187",
-    moment: moment.parseZone("12/21/2020 17:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
-    name: "Start of: Dark Girl Pick Up Gachas",
-    categ: "gacha",
-    future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2199",
-    moment: moment.parseZone("12/21/2020 17:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
-    name: "Shops Expire: Angels On The Road",
-    categ: "shopExpire",
-    future: false,
-    link: null,
-    moment: moment.parseZone("12/23/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
-  },
-  {
     name: "End of: Kimochi Battle - Vigorous Opal Neck",
     categ: "eventEnd",
     future: false,
-    link: "http://jp.rika.ren/pop_up.html#id=2200",
-    moment: moment.parseZone("12/26/2021 15:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+    link: "http://jp.rika.ren/pop_up.html#id=2206",
+    moment: moment.parseZone("12/26/2020 15:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
     name: "End of: Christmas String",
@@ -79,11 +37,53 @@ const events = [
     moment: moment.parseZone("12/28/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
-    name: "Shops expire: Dark Girl Strengthening Pack",
+    name: "Shops Expire: Dark Girl Strengthening Pack",
     categ: "shopExpire",
     future: false,
     link: "http://jp.rika.ren/pop_up.html#id=2197",
     moment: moment.parseZone("12/28/2020 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: December 28 Maintenance",
+    categ: "maintenance",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2215",
+    moment: moment.parseZone("12/28/2020 15:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: December 28 Maintenance",
+    categ: "maintenance",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2215",
+    moment: moment.parseZone("12/28/2020 16:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: Dream Beyond Kefu, Part 1",
+    categ: "eventStart",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2220",
+    moment: moment.parseZone("12/28/2020 16:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: New Year Campaign Bonuses and Re-Run of Chapter Release Missions",
+    categ: "eventStart",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("12/28/2020 16:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: Haregi Gachas",
+    categ: "gacha",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2217",
+    moment: moment.parseZone("12/28/2020 16:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: Countdown Login Bonus",
+    categ: "eventStart",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("12/29/2020 00:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
     name: "Monthly Shop Reset",
@@ -93,11 +93,60 @@ const events = [
     moment: moment.parseZone("12/31/2020 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
   {
+    name: "Start of: Dream Beyond Kefu, Part 2",
+    categ: "eventStart",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("1/1/2021 00:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "Start of: 10 Free Daily Gacha Rolls",
+    categ: "eventStart",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("1/1/2021 00:00 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: Countdown Login Bonus",
+    categ: "eventEnd",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("1/1/2021 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
     name: "Shops Expire: Christmas Costume Tickets, Tsuruno / Felicia Gacha Coins, Touka / Nemu Gacha Coins",
     categ: "shopExpire",
     future: false,
     link: null,
     moment: moment.parseZone("1/4/2021 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: Dream Beyond Kefu",
+    categ: "eventEnd",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2220",
+    moment: moment.parseZone("1/12/2021 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: New Year Campaign Bonuses and Re-Run of Chapter Release Missions",
+    categ: "eventEnd",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("1/12/2021 14:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: 10 Free Daily Gacha Rolls",
+    categ: "eventEnd",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2221",
+    moment: moment.parseZone("1/12/2021 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
+  },
+  {
+    name: "End of: Haregi Gachas",
+    categ: "gacha",
+    future: true,
+    link: "http://jp.rika.ren/pop_up.html#id=2217",
+    moment: moment.parseZone("1/12/2021 23:59 +09", "MM/D/YYYY H:mm ZZ").add(1,'seconds')
   },
 ];
 
